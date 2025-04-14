e0cc380352395efd488004c9ac1076b6d06c6e2f
Chetla Yashwanth <102910783+Chintu9891@users.noreply.github.com>
2025-04-14 19:59:23 +0530

    trip-tracking.component.ts

--- trip-tracking.component.ts
+++ trip-tracking.component.ts
@@ -2,8 +2,8 @@ import { Component} from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  templateUrl: './trip-tracking.component.html',
+  styleUrls: ['./trip-tracking.component.css'],
 })
 export class AppComponent {
   startPoint: string = '';
